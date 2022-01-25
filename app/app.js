39149ce20b89de7665eaa4607d2761830d319135
susukida <susukida@loremipsum.co.jp>
2022-01-25 11:39:14 +0900

    remove add, edit, delete

--- app/app.js
+++ app/app.js
@@ -1,9 +1,8 @@
 const path = require('path')
 const morgan = require('morgan')
 const express = require('express')
-const nocache = require('nocache')
-const model = require('./model')
-const {Op} = model.Sequelize
+const model = require('./model') // <1>
+const {Op} = model.Sequelize // <2>
 
 class App {
   constructor () {
@@ -16,29 +15,17 @@ class App {
     this.router.use(morgan('dev'))
 
     this.router.get('/', (req, res) => res.redirect('./private/todo/'))
-    this.router.get('/private/todo/', this.onRequestPrivateTodoIndex.bind(this))
+    this.router.get('/private/todo/', this.onRequestPrivateTodoIndex.bind(this)) // <3>
     this.router.get('/private/todo/', (req, res) => res.render('todo/private-index'))
     this.router.get('/private/todo/add/', (req, res) => res.render('todo/private-add'))
-    this.router.get('/private/todo/add/finish/', this.onRequestPrivateTodoAddFinish.bind(this))
     this.router.get('/private/todo/add/finish/', (req, res) => res.render('todo/private-add-finish'))
-    this.router.use('/private/todo/:todoId([0-9]+)/', this.onRequestFindTodo.bind(this))
-    this.router.get('/private/todo/:todoId([0-9]+)/', this.onRequestPrivateTodoView.bind(this))
+    this.router.use('/private/todo/:todoId([0-9]+)/', this.onRequestFindTodo.bind(this)) // <4>
+    this.router.get('/private/todo/:todoId([0-9]+)/', this.onRequestPrivateTodoView.bind(this)) // <5>
     this.router.get('/private/todo/:todoId([0-9]+)/', (req, res) => res.render('todo/private-view'))
     this.router.get('/private/todo/:todoId([0-9]+)/edit/', (req, res) => res.render('todo/private-edit'))
     this.router.get('/private/todo/:todoId([0-9]+)/edit/finish/', (req, res) => res.render('todo/private-edit-finish'))
     this.router.get('/private/todo/:todoId([0-9]+)/delete/', (req, res) => res.render('todo/private-delete'))
     this.router.get('/private/todo/delete/finish/', (req, res) => res.render('todo/private-delete-finish'))
-
-    this.router.use('/api/v1/', nocache())
-    this.router.use('/api/v1/', express.json())
-    this.router.get('/api/v1/private/todo/add/initialize', this.onRequestApiV1PrivateTodoAddInitialize.bind(this))
-    this.router.post('/api/v1/private/todo/add/validate', this.onRequestApiV1PrivateTodoAddValidate.bind(this))
-    this.router.post('/api/v1/private/todo/add/submit', this.onRequestApiV1PrivateTodoAddSubmit.bind(this))
-    this.router.use('/api/v1/private/todo/:todoId([0-9]+)/', this.onRequestFindTodo.bind(this))
-    this.router.get('/api/v1/private/todo/:todoId([0-9]+)/edit/initialize', this.onRequestApiV1PrivateTodoEditInitialize.bind(this))
-    this.router.put('/api/v1/private/todo/:todoId([0-9]+)/edit/validate', this.onRequestApiV1PrivateTodoEditValidate.bind(this))
-    this.router.put('/api/v1/private/todo/:todoId([0-9]+)/edit/submit', this.onRequestApiV1PrivateTodoEditSubmit.bind(this))
-    this.router.delete('/api/v1/private/todo/:todoId([0-9]+)/delete/submit', this.onRequestApiV1PrivateTodoDeleteSubmit.bind(this))
 
     this.router.use((req, res) => {
       res.status(404).end()
@@ -54,7 +41,7 @@ class App {
     this.router(req, res)
   }
 
-  async onRequestFindTodo (req, res, next) {
+  async onRequestFindTodo (req, res, next) { // <6>
     try {
       const todo = await model.todo.findOne({
         where: {
@@ -76,7 +63,7 @@ class App {
     }
   }
 
-  async onRequestPrivateTodoIndex (req, res, next) {
+  async onRequestPrivateTodoIndex (req, res, next) { // <7>
     try {
       const todos = await model.todo.findAll({
         order: [['date', 'desc']],
@@ -90,17 +77,7 @@ class App {
     }
   }
 
-  async onRequestPrivateTodoAddFinish (req, res, next) {
-    try {
-      res.locals.id = req.query.id
-
-      next()
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestPrivateTodoView (req, res, next) {
+  async onRequestPrivateTodoView (req, res, next) { // <8>
     try {
       const {todo} = req.locals
       const {date} = todo
@@ -126,154 +103,6 @@ class App {
       }
 
       next()
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestApiV1PrivateTodoAddInitialize (req, res, next) {
-    try {
-      const form = {
-        content: '',
-      }
-
-      const validation = {
-        ok: null,
-        content: {ok: null, isNotEmpty: null},
-      }
-
-      res.send({form, validation})
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestApiV1PrivateTodoAddValidate (req, res, next) {
-    try {
-      const validation = {
-        ok: null,
-        content: {ok: null, isNotEmpty: null},
-      }
-
-      validation.content.isNotEmpty = req.body.form.content !== ''
-      validation.content.ok = validation.content.isNotEmpty
-      validation.ok = validation.content.ok
-
-      res.send({validation})
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestApiV1PrivateTodoAddSubmit (req, res, next) {
-    try {
-      const validation = {
-        ok: null,
-        content: {ok: null, isNotEmpty: null},
-      }
-
-      validation.content.isNotEmpty = req.body.form.content !== ''
-      validation.content.ok = validation.content.isNotEmpty
-      validation.ok = validation.content.ok
-
-      if (!validation.ok) {
-        res.status(400).end()
-        return
-      }
-
-      await model.sequelize.transaction(async (transaction) => {
-        const todo = await model.todo.create({
-          date: new Date(),
-          content: req.body.form.content,
-        }, {transaction})
-
-        const ok = true
-        const redirect = `./finish/?id=${todo.id}`
-
-        res.send({ok, redirect})
-      })
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestApiV1PrivateTodoEditInitialize (req, res, next) {
-    try {
-      const form = {
-        content: req.locals.todo.content,
-      }
-
-      const validation = {
-        ok: null,
-        content: {ok: null, isNotEmpty: null},
-      }
-
-      res.send({form, validation})
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestApiV1PrivateTodoEditValidate (req, res, next) {
-    try {
-      const validation = {
-        ok: null,
-        content: {ok: null, isNotEmpty: null},
-      }
-
-      validation.content.isNotEmpty = req.body.form.content !== ''
-      validation.content.ok = validation.content.isNotEmpty
-      validation.ok = validation.content.ok
-
-      res.send({validation})
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestApiV1PrivateTodoEditSubmit (req, res, next) {
-    try {
-      const validation = {
-        ok: null,
-        content: {ok: null, isNotEmpty: null},
-      }
-
-      validation.content.isNotEmpty = req.body.form.content !== ''
-      validation.content.ok = validation.content.isNotEmpty
-      validation.ok = validation.content.ok
-
-      if (!validation.ok) {
-        res.status(400).end()
-        return
-      }
-
-      await model.sequelize.transaction(async (transaction) => {
-        const {todo} = req.locals
-        const {form} = req.body
-
-        todo.content = form.content
-        await todo.save({transaction})
-
-        const ok = true
-        const redirect = './finish/'
-
-        res.send({ok, redirect})
-      })
-    } catch (err) {
-      next(err)
-    }
-  }
-
-  async onRequestApiV1PrivateTodoDeleteSubmit (req, res, next) {
-    try {
-      await model.sequelize.transaction(async (transaction) => {
-        await req.locals.todo.destroy({transaction})
-
-        const ok = true
-        const redirect = '../../delete/finish/'
-
-        res.send({ok, redirect})
-      })
     } catch (err) {
       next(err)
     }
