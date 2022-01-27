5fcfef7559fe9d613143c1522cfa6bb9b7af82a8
susukida <susukida@loremipsum.co.jp>
2022-01-28 08:39:06 +0900

    add more reference numbers

--- app/app.js
+++ app/app.js
@@ -4,39 +4,39 @@ const express = require('express')
 
 class App {
   constructor () {
-    this.router = express()
+    this.router = express() // <1>
 
-    this.router.set('strict routing', true) // <1>
-    this.router.set('views', path.join(__dirname, 'view'))
-    this.router.set('view engine', 'pug')
+    this.router.set('strict routing', true) // <2>
+    this.router.set('views', path.join(__dirname, 'view')) // <3>
+    this.router.set('view engine', 'pug') // <4>
 
-    this.router.use(morgan('dev')) // <2>
+    this.router.use(morgan('dev')) // <5>
 
-    this.router.get('/', (req, res) => res.redirect('./private/todo/')) // <3>
-    this.router.get('/private/todo/', (req, res) => res.render('todo/private-index'))
+    this.router.get('/', (req, res) => res.redirect('./private/todo/')) // <6>
+    this.router.get('/private/todo/', (req, res) => res.render('todo/private-index')) // <7>
     this.router.get('/private/todo/add/', (req, res) => res.render('todo/private-add'))
     this.router.get('/private/todo/add/finish/', (req, res) => res.render('todo/private-add-finish'))
-    this.router.get('/private/todo/:todoId([0-9]+)/', (req, res) => res.render('todo/private-view'))
+    this.router.get('/private/todo/:todoId([0-9]+)/', (req, res) => res.render('todo/private-view')) // <8>
     this.router.get('/private/todo/:todoId([0-9]+)/edit/', (req, res) => res.render('todo/private-edit'))
     this.router.get('/private/todo/:todoId([0-9]+)/edit/finish/', (req, res) => res.render('todo/private-edit-finish'))
     this.router.get('/private/todo/:todoId([0-9]+)/delete/', (req, res) => res.render('todo/private-delete'))
     this.router.get('/private/todo/delete/finish/', (req, res) => res.render('todo/private-delete-finish'))
 
-    this.router.use((req, res) => { // <4>
-      res.status(404).end()
+    this.router.use((req, res) => { // <9>
+      res.status(404).end() // <10>
     })
 
-    this.router.use((err, req, res, next) => { // <5>
-      res.status(500).end()
-      this.onError(err)
+    this.router.use((err, req, res, next) => { // <11>
+      res.status(500).end() // <12>
+      this.onError(err) // <13>
     })
   }
 
-  onRequest (req, res) { // <6>
+  onRequest (req, res) { // <14>
     this.router(req, res)
   }
 
-  onError (err) { // <7>
+  onError (err) { // <15>
     console.error(err.message)
     console.debug(err.stack)
   }
